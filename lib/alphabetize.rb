078924e65fb14a01802bd2aa057c3b7ea82a32c0
Steven Balasta <sbal13@gmail.com>
2017-06-25 05:35:44 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+
+  arr.sort do |a,b|
+    i = 0
+    while a[i] == b[i]
+      i += 1
+    end
+    esp_alph.index(a[i])<=>esp_alph.index(b[i])
+  end
 end
